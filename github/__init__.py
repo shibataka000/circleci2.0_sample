f0e6ed32c91e5daf80fb51c161dcc60ffe654167
shibataka000 <chise.alter.pasta@gmail.com>
2017-04-29 20:25:25 +0900

    Revert "Embed bug intentionally to see CircleCI'sbehavior."
    This reverts commit 8afb0189807c5412b22d2b71426e3bf856219a1e.

--- github/__init__.py
+++ github/__init__.py
@@ -8,5 +8,4 @@ ROOT_ENDPOINT = "https://api.github.com"
 def get_user(username):
     url = "{}/users/{}".format(ROOT_ENDPOINT, username)
     r = requests.get(url)
-    # return r.json()
-    return {}
+    return r.json()
